67e069540cf02f822410d76efc73089f3f845226
DohyeonYeo <ydh51216@gmail.com>
2022-09-06 23:42:52 +0900

    220906a

---
+++ 220906/02_django/articles/views.py
@@ -0,0 +1,73 @@
+import re
+from django.shortcuts import render, redirect
+from .models import Article
+
+
+# Create your views here.
+def index(request):
+    # DB에 전체 데이터를 조회
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+
+def new(request):
+    return render(request, 'articles/new.html')
+
+
+def create(request):
+    # 사용자의 데이터를 받아서
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    # DB에 저장
+    # 1
+    # article = Article()
+    # article.title = title
+    # article.content = content
+    # article.save()
+
+    # 2
+    article = Article(title=title, content=content)
+    article.save()
+
+    # 3
+    # Article.objects.create(title=title, content=content)
+
+    # return render(request, 'articles/index.html')
+    # return redirect('/articles/')
+    # return redirect('articles:index')
+    return redirect('articles:detail', article.pk)
+
+
+def detail(request, pk):
+    # variable routing으로 받은 pk 값으로 데이터를 조회
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.delete()
+    return redirect('articles:index')
+
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/edit.html', context)
+
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.title = request.POST.get('title')
+    article.content = request.POST.get('content')
+    article.save()
+    return redirect('articles:detail', article.pk)
---
+++ 220906/README.md
@@ -0,0 +1,11 @@
+# 8기 Django 라이브 코드
+
+| Day  | Subject                      |
+| ---- | ---------------------------- |
+| 01   | Django Intro                 |
+| 02   | Django Model                 |
+| 04   | Django Form                  |
+| 05   | Django Authentication System |
+| 07   | Django managing staticfiles  |
+| 08   | Django REST Framework        |
+
