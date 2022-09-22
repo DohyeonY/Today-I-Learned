a22d4f8199a7825ccc902df383b44ccdb5c7aebb
DohyeonYeo <ydh51216@gmail.com>
2022-09-23 01:42:21 +0900

    220923

--- 220922/test.py
+++ 220922/test.py
@@ -1,21 +1,26 @@
-def perm(k, midSum, s) :
-    global minV
-    if k == N :
+# def perm(k, midSum, s) :
+#     global minV
+#     if k == N :
 
 
-        minV = min(minV, minSum)
-        return
-    for i in range(N) :
-        if visited[i] :
-            continue
-        # result[k] = i
-        # s = result[k - 1]
-        visited[i] = True
-        perm(k+1, midSum+ARR[s][i], i)
-        visited[i] = False
-N = 5
-visited = [False] * N
-result = [-1] * N
-visited[0] = True
-result[0] = 0
-perm(1, 0, 0)
+#         minV = min(minV, minSum)
+#         return
+#     for i in range(N) :
+#         if visited[i] :
+#             continue
+#         # result[k] = i
+#         # s = result[k - 1]
+#         visited[i] = True
+#         perm(k+1, midSum+ARR[s][i], i)
+#         visited[i] = False
+# N = 5
+# visited = [False] * N
+# result = [-1] * N
+# visited[0] = True
+# result[0] = 0
+# perm(1, 0, 0)
+
+a = [1, 1, 1, 3]
+setlst = set(a)
+print(a)
+print(setlst)
--- 220922/베이비진.py
+++ 220922/베이비진.py
@@ -1,24 +1,7 @@
-import sys
-sys.stdin=open("베이비진.txt", "r")
+# import sys
+# sys.stdin=open("베이비진.txt", "r")
 
-a = [1, 2, 3]
-b = [2, 3, 4]
-c = [3, 4, 5]
-d = [4, 5, 6]
-e = [5, 6, 7]
-f = [6, 7, 8]
-g = [7, 8, 9]
-aa = [1, 1, 1]
-bb = [2, 2, 2]
-cc = [3, 3, 3]
-dd = [4, 4, 4]
-ee = [5, 5, 5]
-ff = [6, 6, 6]
-gg = [7, 7, 7]
-hh = [8, 8, 8]
-ii = [9, 9, 9]
-jj = [0, 0, 0]
-kk = [0, 1, 2]
+checklst = [[1, 2, 3],[2, 3, 4],[3, 4, 5],[4, 5, 6],[5, 6, 7],[6, 7, 8],[7, 8, 9],[1, 1, 1],[2, 2, 2],[3, 3, 3],[4, 4, 4],[5, 5, 5],[6, 6, 6],[7, 7, 7],[8, 8, 8],[9, 9, 9],[0, 0, 0],[0, 1, 2]]
 
 T = int(input())
 
@@ -34,17 +17,38 @@ for tc in range(1, T+1) :
         playerOne.sort()
         # 정렬 될때마다 + 패가 3장이상일때 있는 패 전부 다 돌려서 하나라도 맞으면 플레이어1 승리
         if len(playerOne) >= 3 :
-            for j in range(0, len(playerOne)-3) :
-                if playerOne[j : j+3] == a or playerOne[j : j+3] == b or playerOne[j : j+3] == c or playerOne[j : j+3] == d or playerOne[j : j+3] == e or playerOne[j : j+3] == f or playerOne[j : j+3] == g or playerOne[j : j+3] == aa or playerOne[j : j+3] == bb or playerOne[j : j+3] == cc or playerOne[j : j+3] == dd or playerOne[j : j+3] == ee or playerOne[j : j+3] == ff or playerOne[j : j+3] == gg or playerOne[j : j+3] == hh or playerOne[j : j+3] == ii or playerOne[j : j+3] == jj or playerOne[j : j+3] == kk :
+            for j in range(0, len(playerOne)-2) :
+                if playerOne[j : j+3] in checklst :
                     result = 1
                     break
+            setlst = set(playerOne)
+            setlst = list(setlst)
+            setlst.sort()
+            if len(setlst) >= 3:
+                for k in range(0, len(setlst)-2) :
+                    if setlst[k : k+3] in checklst :
+                        result = 1
+                        setlst = []
+                        break
+        if result != 0 :
+            break
         playerTwo.append(lst[i+1])
         playerTwo.sort()
         # 같은 카드를 가졌을때 플레이어1이 이기지 못했을때 플레이어2의 패 전부 다 돌려서 하나라도 맞으면 플레이어2 승리
         if len(playerTwo) >= 3 :
-            for j in range(0, len(playerTwo)-3) :
-                if playerTwo[j : j+3] == a or playerTwo[j : j+3] == b or playerTwo[j : j+3] == c or playerTwo[j : j+3] == d or playerTwo[j : j+3] == e or playerTwo[j : j+3] == f or playerTwo[j : j+3] == g or playerTwo[j : j+3] == aa or playerTwo[j : j+3] == bb or playerTwo[j : j+3] == cc or playerTwo[j : j+3] == dd or playerTwo[j : j+3] == ee or playerTwo[j : j+3] == ff or playerTwo[j : j+3] == gg or playerTwo[j : j+3] == hh or playerTwo[j : j+3] == ii or playerTwo[j : j+3] == jj or playerTwo[j : j+3] == kk :
+            for j in range(0, len(playerTwo)-2) :
+                if playerTwo[j : j+3] in checklst :
                     result = 2
                     break
-
+            setlst = set(playerTwo)
+            setlst = list(setlst)
+            setlst.sort()
+            if len(setlst) >= 3:
+                for k in range(0, len(setlst)-2) :
+                    if setlst[k : k+3] in checklst :
+                        result = 2
+                        setlst = []
+                        break
+        if result != 0 :
+            break
     print(f'#{tc} {result}')
