785cd82ba62deffc5606e959b0d81b77df4a5c26
yeodohyeon <ydh51216@gmail.com>
2022-09-23 17:26:31 +0900

    220923_3

---
+++ 220923/디저트카페.py
@@ -0,0 +1,35 @@
+import sys
+sys.stdin=open("디저트카페.txt", "r")
+
+def solve(k, r, c, d):
+    global maxV
+
+    if d == 3 and r == stR and c == stC :
+        if maxV < k :
+            maxV = k
+        return
+
+    if r < 0 or r >= N or c < 0 or c >= N or arr[r][c] in result[:k] :
+        return
+
+    result[k] = arr[r][c]
+    newR, newC =
+    solve(k+1, newR, newC, d)
+    d = (d+1)%
+    newR, newC =
+    solve(k+1, newR, newC, d)
+
+
+
+T = int(input())
+
+for tc in range(1, T+1) :
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    print(arr)
+    maxV = 0
+    for r in range(N) :
+        for c in range(N) :
+            stR, stC = r, c
+            result = [-1] * (4*N)
+            solve(0, r, c, 0)
--- 220923/미생물격리.py
+++ 220923/미생물격리.py
@@ -1,6 +1,41 @@
 import sys
-sys.stdin=open("미생물격리","r")
+sys.stdin=open("미생물격리.txt", "r")
+
+dr = (0, -1, 1, 0, 0)
+dc = (0, 0, 0, -1, 1)
+rev = (0, 2, 1, 4, 3)
 
 T = int(input())
 
 for tc in range(1, T+1) :
+    N, M, K = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(K)]
+
+    for _ in range(M) :
+        for i in range(len(arr)) :
+            row, col, num, dir = arr[i]
+            if not num :                                            # if k의 미생물이 0이면 : continue
+                continue
+            newrow = row + dr[dir]
+            newcol = col + dc[dir]
+            arr[i][0], arr[i][1] = newrow, newcol               # 좌표이동
+            if not (1 <= newrow < N-1 and 1 <= newcol < N-1) :      # 벽이면
+                arr[i][2] = arr[i][2] // 2                      # 미생물값을 반으로
+                arr[i][3] = rev[dir]                              # 방향 전환
+        arr.sort()
+        pre = 0
+    # print(arr)
+        for i in range(1, K) :
+            if arr[i][2] == 0 :
+                continue
+            if arr[i][0] == arr[pre][0] and arr[i][1] == arr[pre][1] :
+                arr[i][2] += arr[pre][2]                             # arr[k]에 미생물 내용에 누적
+                arr[pre][2] = 0
+            pre = i                                                  # pre는 0
+
+    # print(arr)
+    result = 0
+    for k in arr :
+        result += k[2]
+
+    print(f'#{tc} {result}')
