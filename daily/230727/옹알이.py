acb6e83456f2aa8145f10be3b1768024cd3ff23f
DohyeonY <ydh51216@gmail.com>
2023-07-27 23:58:46 +0900

    [ALGO] 프로그래머스 옹알이

---
+++ daily/230727/옹알이.py
@@ -0,0 +1,15 @@
+def solution(babbling) :
+    answer = 0
+    bebe = ['aya', 'ye', 'woo', 'ma']
+    not_bebe = ['ayaaya', 'yeye', 'woowoo', 'mama']
+    for i in babbling : 
+        while i :
+            if i[:2] in bebe and i[:4] not in not_bebe :
+                i = i[:2]
+            elif i[:3] in bebe and i[:6] not in not_bebe :
+                i = i[3:]
+            else :
+                break
+        if not b :
+            answer += 1
+    return answer
