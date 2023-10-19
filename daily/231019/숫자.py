4a0a935b865bb6b29a3b1a3c131d1f4a3a38645d
DohyeonY <ydh51216@gmail.com>
2023-10-19 23:40:29 +0900

    [ALGO] 프로그래머스 숫자표현

---
+++ daily/231019/숫자.py
@@ -0,0 +1,15 @@
+def solution(n):
+    answer = 0
+    result = 0
+
+    for i in range(1, n + 1) :
+        result = 0
+        for j in range(i, n + 1) :
+            result += j
+            if result == n :
+                answer += 1
+                break
+            elif result > n :
+                break 
+    
+    return answer
