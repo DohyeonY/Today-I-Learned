df2bcde779167eefc8680429a5d36c8dabf70474
DohyeonY <ydh51216@gmail.com>
2023-10-31 23:52:31 +0900

    [ALGO] 프로그래머스 점프와순간이동

---
+++ daily/231031/점프와순간이동.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    
+    while n > 0 :
+        if n % 2 == 0 : 
+            n = n // 2
+        else :
+            n = n - 1
+            answer += 1
+    
+    return answer
