d9aca0a4c5586ae95221f8eb9eb133ff07cc0c88
DohyeonY <ydh51216@gmail.com>
2023-11-06 23:57:24 +0900

    [ALGO] 프로그래머스 기능개발

---
+++ daily/231106/기능개발.py
@@ -0,0 +1,20 @@
+def solution(progresses, speeds):
+    answer = []
+    time = 0
+    count = 0 
+    
+    while progresses:
+        if (progresses[0] + time*speeds[0]) >=100 : 
+            progresses.pop(0)
+            speeds.pop(0)
+            count +=1 
+            
+        else :
+            if count > 0 : 
+                answer.append(count) 
+                count = 0
+            time += 1
+            
+    answer.append(count)
+    
+    return answer
